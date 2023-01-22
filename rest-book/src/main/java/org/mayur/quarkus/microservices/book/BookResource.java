aa933fe3ac4c9e5cd5db4046b8f0860782601910
Mkknowledge <mayurkandalkar.it@gmail.com>
2023-01-23 03:48:35 +0530

    Documenting Application using properties

--- rest-book/src/main/java/org/mayur/quarkus/microservices/book/BookResource.java
+++ rest-book/src/main/java/org/mayur/quarkus/microservices/book/BookResource.java
@@ -1,5 +1,7 @@
 package org.mayur.quarkus.microservices.book;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
@@ -9,6 +11,7 @@ import javax.ws.rs.core.Response;
 import java.time.Instant;
 
 @Path("/api/books")
+@Tag(name = "Book REST endpoints")
 public class BookResource {
 
     @Inject
@@ -17,6 +20,10 @@ public class BookResource {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Operation(
+            summary = "Creates a Book",
+            description = "Creates a Book with an ISBN number"
+    )
     public Response createABook(@FormParam("isbn13")String isbn13,
                                 @FormParam("title") String title,
                                 @FormParam("author") String author,
