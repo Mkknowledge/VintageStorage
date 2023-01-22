7ecf6b3aa792c91ad2b6d6fcafe740bd0a3f61de
Mkknowledge <mayurkandalkar.it@gmail.com>
2023-01-23 03:29:34 +0530

    Creation of Book code

--- rest-book/src/main/java/org/mayur/quarkus/microservices/book/BookResource.java
+++ rest-book/src/main/java/org/mayur/quarkus/microservices/book/BookResource.java
@@ -1,16 +1,36 @@
 package org.mayur.quarkus.microservices.book;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import org.jboss.logging.Logger;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.time.Instant;
 
 @Path("/api/books")
 public class BookResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Inject
+    Logger logger;
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response createABook(@FormParam("isbn13")String isbn13,
+                                @FormParam("title") String title,
+                                @FormParam("author") String author,
+                                @FormParam("yearOfPublication") int yearOfPublication,
+                                @FormParam("genre") String genre) {
+
+        Book book = new Book();
+        book.isbn13=isbn13;
+        book.title=title;
+        book.author=author;
+        book.yearOfPublication=yearOfPublication;
+        book.genre=genre;
+        book.creationDate= Instant.now();
+        logger.info("Book created : " + book);
+        return Response.status(201).entity(book).build();
     }
 }
